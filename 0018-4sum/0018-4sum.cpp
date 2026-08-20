8a7e9dc162159aabd9f79943cef88095e39716ef
Rahul Khanna <1rahul1khanna1@gmail.com>
2026-08-20 18:48:23 +0530

    Time: 11 ms (95.69%), Space: 17.5 MB (24.11%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>> ans;
+
+    void fn(vector<int>& nums, long long target, int i, int j, int k, int l){
+        if(j >= k) return;
+        long long sum = nums[i];
+        sum += nums[j];
+        sum += nums[k]; sum += nums[l];
+        if(sum == target){
+            ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+            int x = nums[j], y = nums[k];
+
+            while(j < k && nums[j] == x) j++;
+            while(j < k && nums[k] == y) k--;
+
+            fn(nums, target, i, j, k, l);
+            return;
+        }
+
+        if(sum < target){
+            fn(nums, target, i, j + 1, k, l);
+        }
+        else 
+            fn(nums, target, i, j, k - 1, l);
+    }
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        n = nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for(int l = n - 1; l > i + 2; l--){
+                
+                if(l < n - 1 && nums[l] == nums[l + 1]) continue;
+                fn(nums, target, i, i + 1, l - 1, l);
+            }
+        }
+        return ans;
+
+    }
+};
