5dfad9153cd0e22b68b1a390c1de5386f5f3086e
Rahul Khanna <1rahul1khanna1@gmail.com>
2026-08-27 22:37:13 +0530

    Time: 2 ms (15.06%), Space: 9 MB (11.85%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> words;
+        int left = 0, right = 0;
+
+        for(right; right < s.size(); right++){
+            if(s[right] == ' '){
+                words.push_back(s.substr(left, right - left));
+                left = right + 1;
+            }
+        }
+
+        if(s[right - 1] != ' '){
+            words.push_back(s.substr(left, right - left + 1));
+        }
+        if(words.size() != pattern.size()) return false;
+
+        map<char, string> mp;
+        map<string, char> mp2;
+        int ptr = 0; 
+
+        // for(auto it : words) cout << it << "x" << endl;
+        for(auto it : pattern){
+            cout << it << endl;
+            if(mp.find(it) != mp.end() && mp[it] != words[ptr]) return false;
+            mp[it] = words[ptr++];
+        }
+        
+        ptr = 0;
+        for(auto it : words){
+            cout << it << endl;
+            if(mp2.find(it) != mp2.end() && mp2[it] != pattern[ptr]) return false;
+            mp2[it] = pattern[ptr++];
+        }
+        return true;
+
+    }
+};
