07451843bd8c86f6695069cfdd8ed4be51227abc
Rahul Khanna <1rahul1khanna1@gmail.com>
2026-09-08 06:11:09 +0530

    Time: 22 ms (88.47%), Space: 16.4 MB (70.44%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int dx[4] = {0, -1, 0, 1};
+    int dy[4] = {-1, 0, 1, 0};
+    int m, n;
+    void dfs(vector<vector<char>>& grid, int i, int j){
+        if(i < 0 || i >= m || j < 0 || j >= n ) return;
+        grid[i][j] = '0';
+
+        for(int k = 0; k < 4; k++){
+            int ni = i + dx[k];
+            int nj = j + dy[k];
+
+            if(ni >= 0 && ni < m && nj >= 0 && nj < n && grid[ni][nj] == '1'){
+                dfs(grid, ni, nj);
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        // 2 ways -> DFS and BFS
+
+        m = grid.size();
+        n = grid[0].size();
+        int ans = 0;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    ans++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return ans;
+    }
+};
