6861240e3663c848bacfa1446b47830938af6836
Rahul Khanna <1rahul1khanna1@gmail.com>
2026-09-09 14:39:05 +0530

    Time: 0 ms (100%), Space: 8.2 MB (11.5%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,29 @@
 class Solution {
 public:
-    int fib(int n) {
+    int recursion(int n, vector<int>& dp){
         if(n <= 1) return n;
-        int left = fib(n - 1);
-        int right = fib(n - 2);
-        return left + right;
+        if(dp[n] != -1) return dp[n];
+        int left = recursion(n - 1, dp);
+        int right = recursion(n - 2, dp);
+        return dp[n] =  left + right;
+        // reduces O(2^n) complexity to O(n) itself
+    }
+    int fib(int n) {
+        // simple and works but time complexity is 2^n
+        if(n <= 1) return n;
+        vector<int> dp(n + 1, -1); // -> create a 1d dimension array and initialize it with a value which is unreachable
+        return recursion(n, dp);
+
+        // int left = fib(n - 1);
+        // int right = fib(n - 2);
+        // return left + right;
+
+        // to reduce time complexity we will use DP
+
+        // DP -> RECURSIVE SOLUTION + OVERLAPPING SUBPROBLEM
+        // CONVERT RECURSION -> DP, JUST ADD STORING VALUES INSIDE RECURSION
+
+
+        
     }
 };
